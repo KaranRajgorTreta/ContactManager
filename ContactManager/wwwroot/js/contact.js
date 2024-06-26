efbf8bac2f4e3c7b68d5e9369dfd80254d658738
Karan Rajgor <165757743+KaranRajgorTreta@users.noreply.github.com>
2024-06-26 18:35:40 +0530

    added Jquery AJAX methods for api calling

---
+++ ContactManager/wwwroot/js/contact.js
@@ -0,0 +1,94 @@
+﻿$(document).ready(function () {
+    
+    function createContact(contact) {
+        $.ajax({
+            url: '/Home/CreateContact',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(contact),
+            success: function (response) {
+                if (response.success) {
+                    alert('Contact created successfully. ID: ' + response.data);
+                } else {
+                    alert('Error: ' + response.message);
+                }
+            },
+            error: function (xhr) {
+                alert('An error occurred: ' + xhr.responseText);
+            }
+        });
+    }
+
+    function updateContact(id, contact) {
+        $.ajax({
+            url: '/Home/UpdateContact?id=' + id,
+            type: 'PUT',
+            contentType: 'application/json',
+            data: JSON.stringify(contact),
+            success: function (response) {
+                if (response.success) {
+                    alert('Contact updated successfully.');
+                } else {
+                    alert('Error: ' + response.message);
+                }
+            },
+            error: function (xhr) {
+                alert('An error occurred: ' + xhr.responseText);
+            }
+        });
+    }
+
+    function deleteContact(id) {
+        $.ajax({
+            url: '/Home/DeleteContact?id=' + id,
+            type: 'DELETE',
+            success: function (response) {
+                if (response.success) {
+                    alert('Contact deleted successfully.');
+                } else {
+                    alert('Error: ' + response.message);
+                }
+            },
+            error: function (xhr) {
+                alert('An error occurred: ' + xhr.responseText);
+            }
+        });
+    }
+
+    function viewContact(id) {
+        $.ajax({
+            url: '/Home/ViewContact?id=' + id,
+            type: 'GET',
+            success: function (response) {
+                if (response.success) {
+                    // Display contact details
+                    console.log(response.data);
+                } else {
+                    alert('Error: ' + response.message);
+                }
+            },
+            error: function (xhr) {
+                alert('An error occurred: ' + xhr.responseText);
+            }
+        });
+    }
+
+    function getContactList() {
+        $.ajax({
+            url: '/Home/GetContactList',
+            type: 'GET',
+            success: function (response) {
+                if (response.success) {
+                    // Display contact list
+                    console.log(response.data);
+                } else {
+                    alert('Error: ' + response.message);
+                }
+            },
+            error: function (xhr) {
+                alert('An error occurred: ' + xhr.responseText);
+            }
+        });
+    }
+
+});
